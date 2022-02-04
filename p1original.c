f3d3b23d1ffc3319847c35cd459af942a01d220f
Srinidhi R Bharadwaj <96413310+Srinidhirb@users.noreply.github.com>
2022-02-04 03:48:10 +0000

    p1 orginal

--- p1original.c
+++ p1original.c
@@ -1,2 +1,22 @@
-
+#include<stdio.h>
+void input(int *a, int *b)
+{
+  printf("enter the numbers:\n");
+  scanf("%d%d",a,b);
+}
+void add(int a, int b, int *sum)
+{
+  *sum=a+b;
+}
+void output(int a, int b, int sum)
+{
+  printf("the sum of %d and %d is : %d",a,b,sum);
+}
+int main()
+{
+  int a,b,c;
+  input(&a,&b);
+  add(a,b,&c);
+  output(a,b,c);
+  return 0;
 }
